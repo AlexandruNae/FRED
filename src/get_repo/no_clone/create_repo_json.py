5d0eb9cae6aacaec946b05c22d2bad3eb33fa20d
alexn <alexnaer@gmail.com>
2023-06-21 22:33:19 +0300

    refactor: minor refactor

--- src/get_repo/no_clone/create_repo_json.py
+++ src/get_repo/no_clone/create_repo_json.py
@@ -3,7 +3,7 @@ from no_clone.format_json import transform_data
 from no_clone.create_json import create_json
 
 
-json_file_path = 'repo_json/repository.json'
+json_file_path = '../../repo_json/repository.json'
 create_json(json_file_path)
 fetch_content(json_file_path)
 transform_data()
