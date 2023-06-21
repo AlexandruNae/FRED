c4a43e95fbeb9947fe4cd250663a2419a480bd82
alexn <alexnaer@gmail.com>
2023-06-21 11:38:52 +0300

    refactor(function name): create_or_append_to_json -> create_json

--- src/create_repo_json.py
+++ src/create_repo_json.py
@@ -1,9 +1,9 @@
 from src.get_repo.get_repo_content import fetch_content
 from src.get_repo.format_json import transform_data
-from src.get_repo.create_json import create_or_append_to_json
+from src.get_repo.create_json import create_json
 
 
 json_file_path = 'repository.json'
-create_or_append_to_json(json_file_path)
+create_json(json_file_path)
 fetch_content()
 transform_data()
