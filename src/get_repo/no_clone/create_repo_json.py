db7016d2353aa130ed2076068733c957f75a358b
alexn <alexnaer@gmail.com>
2023-06-24 11:44:30 +0300

    pull request

---
+++ src/get_repo/no_clone/create_repo_json.py
@@ -0,0 +1,9 @@
+from no_clone.get_repo_content import fetch_content
+from no_clone.format_json import transform_data
+from no_clone.create_json import create_json
+
+
+json_file_path = '../../repo_json/repository.json'
+create_json(json_file_path)
+fetch_content(json_file_path)
+transform_data()
