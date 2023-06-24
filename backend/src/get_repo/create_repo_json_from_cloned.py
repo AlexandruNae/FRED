1ba1470ce162d65e7c522f0d9fbae211ceef6e32
alexn <alexnaer@gmail.com>
2023-06-24 11:46:07 +0300

    fix: fixes after pull requests

--- backend/src/get_repo/create_repo_json_from_cloned.py
+++ backend/src/get_repo/create_repo_json_from_cloned.py
@@ -1,12 +1,12 @@
 import os
-from src.get_repo.clone.clone_repo import clone_repo
-from src.get_repo.clone.parse_to_json import load_files
 
+from clone.clone_repo import clone_repo
+from clone.parse_to_json import load_files
 
 repo_owner = os.getenv('repo_owner_apicrypto')
 repo_name = os.getenv('repo_name_apicrypto')
 token = os.getenv('apicrypto_repo')
 folder_path = f"../../cloned_repos/{repo_name}"
 
-# clone_repo(repo_owner, repo_name, token)
+clone_repo(repo_owner, repo_name, token)
 folder_dict = load_files(folder_path, repo_name)
